46d427ae59a90c8d73e93e4730d7265d3fd01b67
Kimo Jr <Kimo Jr@ADMINRG-976HKM7>
2017-03-15 16:25:12 +0100

    1702

---
+++ src/test2/Test2.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test2;
+
+/**
+ *
+ * @author Kimo Jr
+ */
+public class Test2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        static String txtName, txtID, txtClass, txtProgram;
+
+    public static String gettxtID() {
+        return txtID;
+    }
+
+    public static void setID(String ID) {
+        Test2.txtID= ID;
+    }
+
+    public static String getStudName() {
+        return studName;
+    }
+
+    public static void setStudName(String studName) {
+        test2Class.studName = studName;
+    }
+
+    public static String getStudProgram() {
+        return studProgram;
+    }
+
+    public static void setStudProgram(String studProgram) {
+        test2Class.studProgram = studProgram;
+    }
+
+    public static String getStudAGE() {
+        return studAGE;
+    }
+
+    public static void setStudAGE(String studAGE) {
+        test2Class.studAGE = studAGE;
+    }
+
+    public static String getStudClass() {
+        return studClass;
+    }
+
+    public static void setStudClass(String studClass) {
+        test2Class.studClass = studClass;
+    }
+
+    }
+    
+}
